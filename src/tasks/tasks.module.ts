aa210e6d754b5292cfb63d42b19d9a371fefb1f6
ruspian <ruspianntb@gmail.com>
2024-11-02 12:43:24 +0800

    membuat file controller dengan nest cli

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+// menambahkan controller yang kita buat sendiri dengan peritah nest generate controller foldet/nama-controller --flat
+import { TasksController } from './tasks.controller';
+
+@Module({
+  controllers: [TasksController]
+})
 export class TasksModule {}
