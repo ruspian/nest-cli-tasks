eb16f3a475515368c65b058c7b633d3b3778ec6b
ruspian <ruspianntb@gmail.com>
2024-11-02 16:42:04 +0800

    implementasi service dan repository melalui controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,23 +1,30 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dtos/create-tasks.dto';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+  tasksService: TasksService;
+
+  constructor() {
+    this.tasksService = new TasksService();
+  }
+
   @Get()
   listTasks() {
-    return 'ini adalah halaman tasks';
+    return this.tasksService.findAll();
   }
 
   @Post()
   //   menggunakan dekorator @Body untuk mengambil data inputaan dari body/user
   createTask(@Body() body: CreateTaskDto) {
-    return body;
+    return this.tasksService.create(body.content);
   }
 
   @Get('/:id')
 
   //   menggunakan dekorator @Param('id') untuk mengambil id
   getTask(@Param('id') id: string) {
-    return `ini adalah task dengan id: ${id}`;
+    return this.tasksService.findOne(parseInt(id));
   }
 }
