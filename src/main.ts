2a69725250896068ec45140b2d086982f3895813
ruspian <ruspianntb@gmail.com>
2024-11-02 12:37:53 +0800

    membuat file module dengan nest cli

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
+// menambahkan module yang kita buat sendiri dengan peritah nest generate module (nama-module)
+import { TasksModule } from './tasks/tasks.module';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(TasksModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
