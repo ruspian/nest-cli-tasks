e603b92125c32d9735aba019796480e21c93d583
ruspian <ruspianntb@gmail.com>
2024-11-02 13:30:11 +0800

    menambahkan validasi pipe dan cara kerjanya di nest js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 // menambahkan module yang kita buat sendiri dengan peritah nest generate module (nama-module)
 import { TasksModule } from './tasks/tasks.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(TasksModule);
+
+  // menambahkan validasi dengan decorator pipe / pipe adalah validasi
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
