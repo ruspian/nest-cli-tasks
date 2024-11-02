ae614d4edb5170c2ed9ac774dda3f03f749c4f9a
ruspian <ruspianntb@gmail.com>
2024-11-02 16:25:58 +0800

    melanjutkan method pada repository

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,7 +1,10 @@
 import { readFile, writeFile } from 'fs/promises';
 
 export class TasksRepository {
-  async findAll(): Promise<any> {}
+  async findAll(): Promise<any> {
+    const data = await readFile('tasks.json', 'utf-8');
+    return JSON.parse(data);
+  }
 
   async findOne(id: number) {
     const data = await readFile('tasks.json', 'utf-8');
@@ -10,5 +13,17 @@ export class TasksRepository {
     return tasks.find((task: any) => task.id === id);
   }
 
-  async create(tasks: any) {}
+  async create(task: string) {
+    const data = await readFile('tasks.json', 'utf-8');
+    const tasks = JSON.parse(data);
+
+    const newTask = {
+      id: tasks[tasks.length - 1].id + 1,
+      content: task,
+    };
+
+    tasks.push(newTask);
+
+    await writeFile('tasks.json', JSON.stringify(tasks, null, 2), 'utf-8');
+  }
 }
