59e57e9dcc64afaf2927640b278ee51a2baa51d8
ruspian <ruspianntb@gmail.com>
2024-11-02 13:10:18 +0800

    menggunakan postman untuk mencoba berbagai macam method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,18 +2,18 @@ import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
-    @Get()
-    listTasks() {
-        
-    }
+  @Get()
+  listTasks() {
+    return 'ini adalah halaman tasks';
+  }
 
-    @Post()
-    createTask() {
-        
-    }
+  @Post()
+  createTask() {
+    return 'ini adalah halaman create tasks dengan metod POST';
+  }
 
-    @Get('/:id') 
-    getTask() {
-        
-    }
+  @Get('/:id')
+  getTask() {
+    return 'ini adalah halaman id dari tasks';
+  }
 }
